ede2187cba72d12d080e7c3e92c9c3f427e00458
Daniel Lopes <daniel.nunes.meira@outlook.com>
2026-04-21 07:12:38 -0300

    Add initial implementation of Null Object Design Pattern with BankAccount and logging classes

---
+++ CSharpNullObject/Program.cs
@@ -0,0 +1,60 @@
+﻿using static System.Console;
+using Autofac;
+
+public interface ILog
+{
+    void Info(string msg);
+    void Warn(string msg);
+}
+
+class ConsoleLog : ILog
+{
+    public void Info(string msg)
+    {
+        WriteLine(msg);
+    }
+
+    public void Warn(string msg)
+    {
+        WriteLine("WARNING!!! " + msg);
+    }
+}
+
+public class BankAccount
+{
+    private ILog log;
+    private int balance;
+
+    public BankAccount(ILog log)
+    {
+        this.log = log;
+    }
+
+    public void Deposit(int amount)
+    {
+        balance += amount;
+        log?.Info($"Deposited {amount}, balance is now {balance}");
+    }
+}
+
+public class NullLog : ILog
+{
+    public void Info(string msg) { }
+    public void Warn(string msg) { }
+}
+
+class Demo
+{
+    static void Main(string[] args)
+    {
+        var cb = new ContainerBuilder();
+        cb.RegisterType<BankAccount>();
+        cb.RegisterType<NullLog>().As<ILog>();
+
+        using(var c = cb.Build())
+        {
+            var ba = c.Resolve<BankAccount>();
+            ba.Deposit(100);
+        }
+    }
+}
